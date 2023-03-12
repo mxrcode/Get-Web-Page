e026bb369fa640fe7c3e50a3c2a7d93270e55da6
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-03-12 18:28:02 +0200

    Add "referrer" & Add "yargs"

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const puppeteer = require('puppeteer');
+const yargs = require('yargs');
 
-const url = process.argv[2];
-if (!url) {
-    console.log("\n");
-    console.log('\x1b[31m', "Please provide URL as a first argument");
-    console.log('\x1b[32m', "Example: node index.js https://url.com/ timeout userAgent proxy:port username password");
-    console.log('\x1b[32m', "And you can use --no-screenshot, to disable the screenshot function.");
-    console.log('\x1b[0m', '');
-    return;
+const argv = yargs
+    .options({
+        'host': { alias: 'url', type: 'string', demandOption: true, describe: 'URL to navigate to' },
+        't': { alias: 'timeout', type: 'number', describe: 'Timeout in milliseconds', default: 5000 },
+        'ua': { alias: 'user-agent', type: 'string', describe: 'User agent string' },
+        'proxy': { type: 'string', describe: 'Proxy server to use' },
+        'username': { type: 'string', describe: 'Username for proxy authentication' },
+        'password': { type: 'string', describe: 'Password for proxy authentication' },
+        'screenshot': { type: 'boolean', describe: 'Whether to take a screenshot', default: false },
+        'screenshot-path': { type: 'string', describe: 'Path to save the screenshot', default: 'screenshot.png' },
+        'html-path': { type: 'string', describe: 'Path to save the page HTML', default: 'page.html' },
+        'ref': { alias: 'referer', type: 'string', describe: 'Value for the Referrer header' }
+    })
+    .help()
+    .argv;
+
+if (!argv.url || argv.help) {
+    yargs.showHelp();
+    process.exit();
 }
-let timeout = process.argv[3];
-if (!timeout) {
-    timeout = 5000;
-}
-const userAgent = process.argv[4];
-const proxy = process.argv[5];
-const username = process.argv[6];
-const password = process.argv[7];
-const screenshotPath = process.argv[8] || 'screenshot.png';
-const pageHtmlPath = process.argv[9] || 'page.html';
-const takeScreenshot = process.argv.includes('--screenshot');
 
 async function run () {
-    let browser;
-    if (proxy) {
-        browser = await puppeteer.launch({
-            args: [`--proxy-server=socks5://${proxy}`, '--no-sandbox']
-        });
-    } else {
-        browser = await puppeteer.launch();
-    }
+    const url = argv.url;
+    const timeout = argv.timeout;
+    const userAgent = argv['user-agent'];
+    const proxy = argv.proxy;
+    const username = argv.username;
+    const password = argv.password;
+    const takeScreenshot = argv.screenshot;
+    const screenshotPath = argv['screenshot-path'];
+    const pageHtmlPath = argv['html-path'];
+    const referrer = argv.referrer;
+
+    const browser = await puppeteer.launch({ args: proxy ? [`--proxy-server=${proxy}`, '--no-sandbox'] : [] });
     const page = await browser.newPage();
+
     if (username && password) {
         await page.authenticate({ username, password });
     }
@@ -42,7 +48,13 @@ async function run () {
         height: 1080,
         deviceScaleFactor: 1
     });
-    await page.goto(url);
+    if (referrer) {
+        await page.setExtraHTTPHeaders({ 'Referer': referrer });
+    }
+    await page.goto(url, { 
+    waitUntil: 'networkidle0', // Wait for the page to load
+    followRedirects: false // Don't follow redirects
+    });
     await page.waitForTimeout(timeout);
     if (takeScreenshot) {
         await page.screenshot({
